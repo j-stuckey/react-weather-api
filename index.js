d644f79179cb10137175bf82bcd733cb9f3fe894
James Stuckey <jstuckey@rutgers.edu>
2020-03-28 21:06:31 -0400

    Request working

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
-const server = app.listen(port = 8080, () => {
-    console.info(`App listening on port ${server.address().port}`)
+app.use(express.json());
+
+app.use(cors({ origin: '*' }));
+
+app.get('/api/weather', (req, res) => {
+    const { location } = req.query;
+
+    res.json({ message: 'OK' });
+});
+
+const server = app.listen((port = 8080), () => {
+    console.info(`App listening on port ${server.address().port}`);
 });
